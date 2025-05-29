c79c26c6750c656f1410b9f4a294403195ac5584
Flowers for Tuesday <xuuumz@gmail.com>
2025-05-29 18:38:03 +0800

    Delete Title directory

--- Title/draft.py
+++
@@ -1,7 +0,0 @@
-from manim import *
-
-class TestScene(Scene):
-    def construct(self):
-        text = Text("混沌", font='Microsoft YaHei Light')
-        self.play(Write(text))
-        self.wait(1)
